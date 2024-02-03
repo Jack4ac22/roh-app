69946eb7ad3f19cbd362b15f6067f90c452360ff
Jack Kazanjyan <kazanjian.jack.z@gmail.com>
2024-02-03 16:49:45 +0100

    init

---
+++ app/page.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main>
+      <section>
+        <h1>Reason Of Hope</h1>
+        <p>
+          Working together to bring the Biblical resources to the Arabic
+          speaking world.
+        </p>
+      </section>
+    </main>
+  );
+}
