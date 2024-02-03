a3f7cd1149874680e338c4d51126d995afd78155
Jack Kazanjyan <kazanjian.jack.z@gmail.com>
2024-02-03 17:07:10 +0100

    Add test_env variable to Home component

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const test_env = process.env.TEST_ENV;
   return (
     <main>
       <section>
@@ -9,6 +10,9 @@ export default function Home() {
         <p>
           Working together to bring the Biblical resources to the Arabic
           speaking world.
+        </p>
+        <p>
+          test_env: {test_env ? test_env : "undefined"}
         </p>
       </section>
     </main>
